b04674537ecc745cb50216496678b4278adc87a3
Shawn Presser <shawnpresser@gmail.com>
2019-10-16 00:17:51 -0500

    Delete intermediate files on startup

--- modules/create/parse.js
+++ modules/create/parse.js
@@ -3,7 +3,26 @@ import { encode } from 'base64-arraybuffer';
 import shortid from 'shortid';
 
 
+export const wipe = async () => {
+  const path = `${FileSystem.documentDirectory}`;
+  const files = await FileSystem.readDirectoryAsync(path);
+  for (let file of files) {
+    console.log('Checking', file);
+    if (file.startsWith(`intermediate_`) && file.endsWith('.png')) {
+      const fullPath = `${FileSystem.documentDirectory}${file}`;
+      console.log('Wiping', fullPath);
+      await FileSystem.deleteAsync(fullPath);
+    }
+  }
+};
+
+let shouldWipe = true;
+
 export const image = async (imageBuffer) => {
+  if (shouldWipe) {
+    shouldWipe = false;
+    await wipe();
+  }
   const base64 = encode(imageBuffer);
   const str = shortid.generate();
   const path = `${FileSystem.documentDirectory}intermediate_${str}.png`;
