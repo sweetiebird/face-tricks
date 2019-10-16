ed336771ae9e68f7d88abf12d5d86519ef39d0d5
Shawn Presser <shawnpresser@gmail.com>
2019-10-15 20:49:48 -0500

    Add save result button

--- modules/create/components/EditScreen/components/ResultImagePreview/ResultImagePreview.js
+++ modules/create/components/EditScreen/components/ResultImagePreview/ResultImagePreview.js
@@ -4,6 +4,13 @@ import { Dimensions, View } from 'react-native';
 
 import { PreviewImage } from 'components';
 
+import { CameraRoll } from 'react-native'
+
+import { colors, create, editorKeys, icons } from 'constants';
+import {
+  TextWithTinyButton,
+} from 'components';
+
 const ResultImagePreview = ({ results }) => {
   const { width: size } = Dimensions.get('window');
 
@@ -21,11 +28,34 @@ const ResultImagePreview = ({ results }) => {
         marginBottom: 20,
       }}
     >
-      {results.map(uri => (
-        <PreviewImage
-          key={uri}
-          uri={uri}
-        />
+      {results.map((uri, i) => (
+        <React.Fragment>
+          { (i >= results.length - 1) ? (
+              <TextWithTinyButton
+                buttonProps={{
+                  icon: icons.CREATE,
+                  iconColor: colors.white,
+                  isPrimary: true,
+                  onPress: () => {
+                    console.log('saving', uri);
+                    CameraRoll.saveToCameraRoll(uri);
+                  },
+                }}
+                textProps={{
+                  italic: true,
+                  style: { marginRight: 8 }
+                }}
+              >
+                Save Result
+              </TextWithTinyButton>
+            ) : null }
+          { (i >= results.length - 1) ? (
+              <PreviewImage
+                key={uri}
+                uri={uri}
+              />
+            ) : null }
+        </React.Fragment>
       ))}
     </View>
   );
