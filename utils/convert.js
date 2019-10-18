aaad4b0e070de58364dd9b9f6c83613b539fe36c
Shawn Presser <shawnpresser@gmail.com>
2019-10-17 21:57:41 -0500

    Define uniqueId, randString, and randb

--- utils/convert.js
+++ utils/convert.js
@@ -16,3 +16,37 @@ export const mapconcat = (f, xs, sep = " ") => {
   }
   return r;
 };
+
+import * as Random from 'expo-random';
+
+export const randb = async () => {
+  const randomBytes = await Random.getRandomBytesAsync(1);
+  return randomBytes[0];
+};
+
+const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+export const randString = async (n) => {
+  const nc = alphabet.length;
+  let s = "";
+  while (s.length < n) {
+    const x = await randb();
+    if (x <= 247) {
+      const c = alphabet[x % nc];
+      s += c;
+    }
+  }
+  return s;
+};
+
+const uniqueIds = {};
+
+export const uniqueId = async (n) => {
+  const id = await randString(Math.max(5, n));
+  if (uniqueIds[id]) {
+    return uniqueId(n);
+  } else {
+    uniqueIds[id] = id;
+    return id;
+  }
+};
